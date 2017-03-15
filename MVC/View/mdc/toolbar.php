a351e62b279f3eb47634d0534102fc1c0a906292
Mateus B. Cassiano <mbc07@live.com>
2017-03-15 13:28:48 -0400

    MDC

---
+++ MVC/View/mdc/toolbar.php
@@ -0,0 +1,20 @@
+<header class="mdc-toolbar mdc-toolbar--fixed">
+	<section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+		<span class="mdc-toolbar__title">CSS Tool</span>
+	</section>
+	
+	<section class="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
+		<a class="material-icons app-toolbar-user-button" aria-label="Download" alt="Download">account_circle</a>
+	</section>
+
+	<div class="mdc-simple-menu mdc-simple-menu--open-from-top-right" style="top: 1em; right: 1em;" tabindex="-1">
+		<ul class="mdc-simple-menu__items mdc-list" role="menu" aria-hidden="true">
+			<li class="mdc-list-item" role="menuitem" tabindex="0"><i class="material-icons">face</i>Fulano Beltrano</li>
+			<li class="mdc-list-divider" role="separator"></li>
+			<li class="mdc-list-item" role="menuitem" tabindex="0">Sair</li>
+			<li class="mdc-list-item" role="menuitem" tabindex="0">Configurações</li>
+		</ul>
+	</div>
+
+	<script src="assets/app-toolbar.js" charset="utf-8"></script>
+</header>
