fb617f2c49876a88204d8210071628a217ea2512
lucasgian <gianextensao@gmail.com>
2017-03-15 12:25:10 -0400

    Modelo de Desenvolvimento

---
+++ MVC/View/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html class="mdc-typography">
+    <head>
+    	<meta charset="utf-8">
+
+        <title>CSS Tool</title>
+        
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+		
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div id='move-of-page' class='nav-page'>
+                	<a class="navbar-brand" href="home.php">Home</a>
+                	<a class="navbar-brand" href="tool.php">Tool</a>
+                	<a class="navbar-brand" href="about.php">About</a>
+               		
+                	<a id='position-login' class="navbar-brand" href="login.php">Login</a>
+                	<a id='position-register' class="position-register navbar-brand" href="register.php">Register</a>
+                </div>
+            </div>
+            
+            <div id="navbar" class="navbar-collapse collapse">
+   				<ul class="nav navbar-nav">
+    			</ul>           
+            </div><!--/.navbar-collapse -->
+        </div>
+    </nav>
+
+   	<div class="container">
---
+++ MVC/View/home.php
@@ -0,0 +1,49 @@
+<?php require_once 'header.php'; ?>
+
+  <body>
+  	<?php require_once 'carousel.php'; ?>
+  	
+    
+     <div id='title-h1'>
+     	<h1>Escopo</h1>
+     </div>
+     
+     <div>
+	     <ul>
+			<li>CSS Auto Generator - Gerador automático CSS.</li>
+			<li>
+				Gera uma folha de estilos CSS de acordo com a formatação feita pelo usuário (Ex. Centralizar div, alterar cor de table para Black). 
+			</li>
+			<li>
+				O produto, depois do processo do cadastramento do usuário e login para permissão do uso da ferramenta, tem a funcionalidade de
+				<ul> 
+					<br>
+					<li>
+						fazer upload do arquivo de HTML, na qual o usuário quer para gerar o CSS;
+					</li>
+					<li>
+						oferecer uma “lista” de funções de CSS disponível para ser inserido no arquivo CSS, dependendo da tag;
+					</li>
+					<li>
+						oferecer um pré-visualização do layout do HTML com o CSS, inserido até o momento;
+					</li>
+					<li>
+						oferecer índice das tags do HTML upado, para melhorar a navegação das partes modificadas;
+					</li>
+					<li>
+						oferecer um botão de confirmação das inserções que irá gerar um arquivo compactado do CSS gerado.
+					</li>
+					<br>
+				</ul>
+			</li>
+			<br>
+			<li>
+				As melhorias de processo tem como visão a modularização e otimização das já existentes, isso implica em ferramentas mais eficientes em todas as faces, inclusive no desenvolvimento, onde a gama de API é grande e auxiliadora nas mais distintas tarefas; uma importante parte do desenvolvimento web é a estilização e isso torna a ferramenta de criação CSS em um potencial para novos desenvolvedores ou padronização no desenvolvimento dos designs de estilo das páginas.
+
+			</li>
+		</ul>
+	</div>
+
+    
+ 
+<?php require_once 'footer.php'; ?>
