220e2092a24ce015d1b54d6b01205cf1dd1580dc
Mateus B. Cassiano <mbc07@live.com>
2017-03-26 14:02:58 -0400

    Exemplo do footer MDL

--- View/fragments/content.php
+++ View/fragments/content.php
@@ -1,27 +1,29 @@
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-tabs">
-	<?php include 'navbar.php'; ?>
+    <?php include 'navbar.php'; ?>
 
-	<main class="mdl-layout__content">
-	    <section class="mdl-layout__tab-panel is-active" id="home">
-	        <div class="page-content">
+    <main class="mdl-layout__content">
+        <section class="mdl-layout__tab-panel is-active" id="home">
+            <div class="page-content">
 
-	            <!-- TO DO: Refazer a home -->
-	            <?php include 'home.php'; ?>
+                <!-- TO DO: Refazer a home -->
+                <?php include 'home.php'; ?>
 
-	        </div>
-	    </section>
-	    <section class="mdl-layout__tab-panel" id="tool">
-	        <div class="page-content">
+            </div>
+        </section>
+        <section class="mdl-layout__tab-panel" id="tool">
+            <div class="page-content">
 
-	            <?php include 'tool.php'; ?>
+                <?php include 'tool.php'; ?>
 
-	        </div>
-	    </section>
-	    <section class="mdl-layout__tab-panel" id="about">
+            </div>
+        </section>
+        <section class="mdl-layout__tab-panel" id="about">
 
-	        <!-- TO DO: Refazer o sobre -->
-	        <?php include 'about.php'; ?>
+            <!-- TO DO: Refazer o sobre -->
+            <?php include 'about.php'; ?>
 
-	    </section>
-	</main>
+        </section>
+    </main>
+
+    <?php include 'footer.php'; ?>
 </div>
--- View/fragments/footer.php
+++ View/fragments/footer.php
@@ -1,2 +1,65 @@
+<!-- Mini Footer -->
+<footer class="mdl-mini-footer">
+  <div class="mdl-mini-footer__left-section">
+    <div class="mdl-logo">
+      More Information
+    </div>
+    <ul class="mdl-mini-footer__link-list">
+      <li><a href="#">Help</a></li>
+      <li><a href="#">Privacy and Terms</a></li>
+      <li><a href="#">User Agreement</a></li>
+    </ul>
+  </div>
+  <div class="mdl-mini-footer__right-section">
+    <button class="mdl-mini-footer__social-btn"></button>
+    <button class="mdl-mini-footer__social-btn"></button>
+    <button class="mdl-mini-footer__social-btn"></button>
+  </div>
+</footer>
 
- 
+<!-- Mega Footer
+<footer class="mdl-mega-footer">
+  <div class="mdl-mega-footer__top-section">
+    <div class="mdl-mega-footer__left-section">
+      <button class="mdl-mega-footer__social-btn"></button>
+      <button class="mdl-mega-footer__social-btn"></button>
+      <button class="mdl-mega-footer__social-btn"></button>
+    </div>
+    <div class="mdl-mega-footer__right-section">
+      <a href="#">Introduction</a>
+      <a href="#">App Status Dashboard</a>
+      <a href="#">Terms of Service</a>
+    </div>
+  </div>
+  <div class="mdl-mega-footer__middle-section">
+    <div class="mdl-mega-footer__drop-down-section">
+      <h1 class="mdl-mega-footer__heading">Learning and Support</h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">Resource Center</a></li>
+        <li><a href="#">Help Center</a></li>
+        <li><a href="#">Community</a></li>
+        <li><a href="#">Learn with Google</a></li>
+        <li><a href="#">Small Business Community</a></li>
+        <li><a href="#">Think Insights</a></li>
+      </ul>
+    </div>
+    <div class="mdl-mega-footer__drop-down-section">
+      <h1 class="mdl-mega-footer__heading">Just for Developers</h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">Google Developers</a></li>
+        <li><a href="#">AdWords API</a></li>
+        <li><a href="#">AdWords Scipts</a></li>
+        <li><a href="#">AdWords Remarketing Tag</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="mdl-mega-footer__bottom-section">
+    <div class="mdl-logo">
+      More Information
+    </div>
+    <ul class="mdl-mega-footer__link-list">
+      <li><a href="#">Help</a></li>
+      <li><a href="#">Privacy and Terms</a></li>
+    </ul>
+  </div>
+</footer> -->
